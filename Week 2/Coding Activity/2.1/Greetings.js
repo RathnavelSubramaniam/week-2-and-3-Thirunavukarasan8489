6fc55877b77abe81cb2248f6d3b10512648bd545
Thirunavukarasan8489 <91112083+Thirunavukarasan8489@users.noreply.github.com>
2021-10-20 11:22:17 +0530

    Add files via upload

---
+++ Week 2/Coding Activity/2.1/Greetings.js
@@ -0,0 +1,21 @@
+/*Instructions
+ - Greet your friend by printing a message to the console.
+*/
+//your code here
+var useName="myfriend";
+///////////////
+function greetings() {
+  //also your code here
+  ///////////////
+  greetings='Thiru'
+  return greetings
+}
+
+//leave this line unchanged to console log the results
+console.log('results: ', greetings());
+
+//don't change this line
+if (typeof module !== 'undefined') {
+  module.exports = greetings;
+}
+
